1cf1948d5d1833c1568a3cfd7ab75331397bbb92
Vivek Viswam R V <vivek.viswam@bigbinary.com>
2022-09-22 20:45:53 +0530

    Replaced project name with neetoUI instead of Wheel

--- app/javascript/src/components/Common/Sidebar/index.jsx
+++ app/javascript/src/components/Common/Sidebar/index.jsx
@@ -50,7 +50,7 @@ const Sidebar = () => {
       changelogProps={{ id: "neetochangelog-trigger" }}
       navLinks={SIDENAV_LINKS}
       organizationInfo={{
-        name: "Wheel",
+        name: "neetoUI",
         subdomain: "bigbinary.com",
       }}
       profileInfo={{
