a8213d6150d98aba285e139307e24aaa34f7b692
Vivek Viswam R V <vivek.viswam@bigbinary.com>
2022-09-24 18:05:59 +0530

    Removed an obsolete constant

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -9,18 +9,3 @@ export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
   title: yup.string().required("Title is required"),
   description: yup.string().required("Description is required"),
 });
-
-export const NOTES_TABLE_COLUMN_DATA = [
-  {
-    title: "Title",
-    dataIndex: "title",
-    key: "title",
-    width: "30%",
-  },
-  {
-    title: "Description",
-    dataIndex: "description",
-    key: "description",
-    width: "70%",
-  },
-];
